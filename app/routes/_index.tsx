fd3c0fc1274d847394280a3c0b57ab3d9c3a18f7
Fernando Lucio Canizo <fernandocanizo@gmail.com>
2024-04-09 10:13:56 -0400

    Consume SSE to show a timer

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,31 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node"
+
+import { json } from "@remix-run/node"
+import { useLoaderData } from "@remix-run/react"
+import { useEventSource } from "remix-utils/sse/react"
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
-};
+    { title: "SSE Demo" },
+    { name: "description", content: "A pet project to learn server-sent events with Remix" },
+  ]
+}
 
-export default function Index() {
+export async function loader() {
+  return json({ time: new Date().toISOString() })
+}
+
+export default function Time() {
+  const loaderData = useLoaderData<typeof loader>()
+  const time = useEventSource("/sse-time", { event: "time" }) ?? loaderData.time
+
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+    <time dateTime={time}>
+      {new Date(time).toLocaleTimeString("en", {
+        minute: "2-digit",
+        second: "2-digit",
+        hour: "2-digit",
+      })}
+    </time>
+  )
 }
